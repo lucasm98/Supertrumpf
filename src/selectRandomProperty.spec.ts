55d94950e71a7c9e3a218a899f860f17dcf8d15f
lucasm98 <lucasmartin13@yahoo.de>
2022-04-03 19:45:01 +0200

    added first test for selectRandomProperty

---
+++ src/selectRandomProperty.spec.ts
@@ -0,0 +1,14 @@
+import selectRandomProperty from './selectRandomProperty';
+
+describe('selectRandomPrperty', () => {
+  it('should return the string "age"', () => {
+    const originalRandom = global.Math.random;
+    global.Math.random = jest.fn().mockReturnValue(0.41);
+
+    const result = selectRandomProperty();
+
+    expect(result).toBe('age');
+    expect(global.Math.random).toHaveBeenCalled();
+    global.Math.random = originalRandom;
+  });
+});

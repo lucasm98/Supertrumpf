de954baf8f7a6d1a777c0f9584669b60a1ee3d35
Lucas <lucasmartin13@yahoo.de>
2022-04-16 18:36:35 +0200

    added Grid, delete- and edit Button to list

--- src/admin/List.tsx
+++ src/admin/List.tsx
@@ -1,6 +1,10 @@
-import React, {ChangeEvent, useState} from "react";
-import { Paper, Table ,TableHead, TableRow, TableCell, TableBody, TextField, TableSortLabel } from '@material-ui/core';
+import React, {ChangeEvent, useState, useCallback, MouseEvent} from "react";
+import { Paper, Table ,TableHead, TableRow, TableCell, TableBody, TextField, TableSortLabel, Grid, Hidden } from '@material-ui/core';
 import Animal from "../game/Animal";
+
+import {IconButton} from "@material-ui/core";
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 interface Props {
   animals: Animal[];
@@ -26,70 +30,88 @@ export default function List({animals}: Props) {
   };
 
   return (
-    <Paper>
-      <TextField
-        label = "Liste filtern"
-        value = {filter}
-        onChange={(e: ChangeEvent<HTMLInputElement>) =>
-          setFilter(e.currentTarget.value)
-        }
-      />
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <TableSortLabel
-                active={sort.orderBy === 'name'}
-                direction={sort.order}
-                onClick={createSortHandler('name')}
-              >
-                Name
-              </TableSortLabel>
-            </TableCell>
-            {(Object.keys(Animal.properties) as (keyof Animal)[]).map(property => (
-              <TableCell align="right" key={property}>
-                <TableSortLabel
-                  active={sort.orderBy === property}
-                  direction={sort.order}
-                  onClick={createSortHandler(property)}
-                >
-                  {Animal.properties[property].label}
-                  {Animal.properties[property].unit !== '' &&
-                    ' ('+ Animal.properties[property].unit + ')'}
-                </TableSortLabel>
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {animals
-            .filter(animal =>
-              animal.name.toLowerCase().includes(filter.toLowerCase())
-            )
-            .sort((animalA: Animal, animalB: Animal) => {
-              let result = 0;
-              if(animalB[sort.orderBy]! < animalA[sort.orderBy]!) {
-                result = -1;
-              }
-              if(animalB[sort.orderBy]! > animalA[sort.orderBy]!) {
-                result = 1;
-              }
-              return sort.order === 'asc' ? result * -1 : result;
-            })
-            .map(animal => (
-            <TableRow key={animal.id}>
-              <TableCell>{animal.name}</TableCell>
-              {(Object.keys(Animal.properties) as (keyof Animal)[]).map(
-                property => (
-                  <TableCell key={property} align="right">
-                    {animal[property]}
+    <Grid container>
+      <Grid item md={1} />
+      <Grid item xs={12} md={10}>
+        <Paper>
+          <TextField
+            label = "Liste filtern"
+            value = {filter}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setFilter(e.currentTarget.value)
+            }
+          />
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <TableSortLabel
+                    active={sort.orderBy === 'name'}
+                    direction={sort.order}
+                    onClick={createSortHandler('name')}
+                  >
+                    Name
+                  </TableSortLabel>
+                </TableCell>
+                {(Object.keys(Animal.properties) as (keyof Animal)[]).map(property => (
+                  <TableCell align="right" key={property}>
+                    <TableSortLabel
+                      active={sort.orderBy === property}
+                      direction={sort.order}
+                      onClick={createSortHandler(property)}
+                    >
+                      {Animal.properties[property].label}
+                      {Animal.properties[property].unit !== '' &&
+                        ' ('+ Animal.properties[property].unit + ')'}
+                    </TableSortLabel>
                   </TableCell>
+                ))}
+                <TableCell>{/*edit*/}</TableCell>
+                <TableCell>{/*delete*/}</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {animals
+                .filter(animal =>
+                  animal.name.toLowerCase().includes(filter.toLowerCase())
                 )
-              )}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
+                .sort((animalA: Animal, animalB: Animal) => {
+                  let result = 0;
+                  if(animalB[sort.orderBy]! < animalA[sort.orderBy]!) {
+                    result = -1;
+                  }
+                  if(animalB[sort.orderBy]! > animalA[sort.orderBy]!) {
+                    result = 1;
+                  }
+                  return sort.order === 'asc' ? result * -1 : result;
+                })
+                .map(animal => (
+                  <TableRow key={animal.id}>
+                    <TableCell>{animal.name}</TableCell>
+                    {(Object.keys(Animal.properties) as (keyof Animal)[]).map(
+                      property => (
+                        <TableCell key={property} align="right">
+                          {animal[property]}
+                        </TableCell>
+                      )
+                    )}
+                    <TableCell>
+                      <IconButton>
+                        <DeleteIcon/>
+                      </IconButton>
+                    </TableCell>
+                    <TableCell>
+                      <IconButton>
+                        <EditIcon/>
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        </Paper>
+      </Grid>
+      <Grid item md={1} />
+    </Grid>
   );
 }

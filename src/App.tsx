9ccf2f1216e3839c71cb3bc53e673bf5a0be4900
Lucas <lucasmartin13@yahoo.de>
2022-04-18 17:20:16 +0200

    added Routing to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Admin from './admin/Admin';
+import Game from './game/Game';
+import Login from './login/Login';
 
 export default function App() {
-  return <Admin />;
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path="/"  element={<Login onLogin={(username, userpassword) => {}} error="" />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/game" element={<Game title="Supertrunpf" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }

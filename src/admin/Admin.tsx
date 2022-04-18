4cbed1f242c2d86f1f884a1d041d9f96739511a3
Lucas <lucasmartin13@yahoo.de>
2022-04-18 11:46:48 +0200

    makeshift solution for post method without fileupload

--- src/admin/Admin.tsx
+++ src/admin/Admin.tsx
@@ -22,25 +22,31 @@ export default function Admin() {
   };
 
   const saveAnimal = async (animal: Animal) => {
-    const data = new FormData();
+/*    const data = new FormData();
     data.append('name', animal.name);
     data.append('image', animal.image);
     data.append('size', animal.size.toString());
     data.append('weight', animal.weight.toString());
     data.append('age', animal.age.toString());
     data.append('offspring', animal.offspring.toString());
-    data.append('speed', animal.speed.toString());
-    console.log(data);
+    data.append('speed', animal.speed.toString());*/
+
+    const data = {
+      'name': animal.name,
+      'image': "placeholder.png",//animal.image,
+      'size': animal.size,
+      'weight': animal.weight,
+      'age': animal.age,
+      'offspring': animal.offspring,
+      'speed': animal.speed,
+    }
+
     if(animal.id) {
       const updateAnimal = await axios.put(
         `http://localhost:3001/card/${animal.id}`,
-        data,
-        {
-          headers: {
-            'content-type': 'multipart/form-data',
-          },
-        }
+        data
       );
+
       setAnimals(animals => {
         const index = animals.findIndex(
           animal => animal.id === updateAnimal.data.id
@@ -48,11 +54,8 @@ export default function Admin() {
         return update(animals, { [index]: { $set: updateAnimal.data } });
       });
     } else {
-      const newAnimal = await axios.post('http://localhost:3001/card', data, {
-        headers: {
-          'content-type': 'multipart/form-data',
-        },
-      });
+      const newAnimal = await axios.post('http://localhost:3001/card', data);
+
       setAnimals(animals => update(animals, {$push: [newAnimal.data]}));
     }
   };
